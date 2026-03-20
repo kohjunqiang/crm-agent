cd90b37dc1124cc716372560f78a457953ebed89
kohjunqiang <kohjunqiang@gmail.com>
2026-03-20 18:02:20 +0800

    remove past change

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@agent-crm/shared'],
-  outputFileTracingRoot: path.join(__dirname, '../../'),
 };
 
 module.exports = nextConfig;
--- apps/web/src/app/(dashboard)/test/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function TestPage() {
-  return <div>It works!</div>;
-}
